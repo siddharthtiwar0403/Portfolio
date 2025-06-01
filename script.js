114284cdde5db2471caa5377d3e868ad70649002
Siddharth Tiwari <150476061+siddharthtiwar0403@users.noreply.github.com>
2025-06-01 12:08:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,48 +1,51 @@
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
 
-        var tablinks = document.getElementsByClassName("tab-links");
-        var tabcontents = document.getElementsByClassName("tab-contents");
+function opentab(tabname) {
+  for (tablink of tablinks) {
+    tablink.classList.remove("active-link");
+  }
+  for (tabcontent of tabcontents) {
+    tabcontent.classList.remove("active-tab");
+  }
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
+}
+var sidemenu = document.getElementById("sidemenu");
+var overlay = document.getElementById("overlay");
+function openmenu() {
+  sidemenu.classList.add("show");
+  overlay.style.display = "block";
+}
+function closemenu() {
+  sidemenu.classList.remove("show");
+  overlay.style.display = "none";
+}
 
-        function opentab(tabname) {
-            for(tablink of tablinks) {
-                tablink.classList.remove("active-link");
-            }
-            for(tabcontent of tabcontents) {
-                tabcontent.classList.remove("active-tab");
-            }
-            event.currentTarget.classList.add("active-link");
-            document.getElementById(tabname).classList.add("active-tab");
-        }
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbxJ4Q8zCDxM2r2YHIH7zOKThCkbR5oM4nuxjImvFVekSocZtEaeMbvJQ6b55U9MZQ1H/exec";
+const form = document.forms["submit-to-google-sheet"];
+const msg = document.getElementById("msg");
 
-      var sidemenu = document.getElementById("sidemenu")
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      msg.innerHTML = "Imformation sent to Siddharth Tiwari";
+      setTimeout(function () {
+        msg.innerHTML = " ";
+      }, 1000);
+      form.reset();
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
 
-      function openmenu(){
-        sidemenu.style.width = "200px"
-      }
-      function closemenu(){
-        sidemenu.style.width = "0px"
-      }
-
-
-
-      const scriptURL = 'https://script.google.com/macros/s/AKfycbxJ4Q8zCDxM2r2YHIH7zOKThCkbR5oM4nuxjImvFVekSocZtEaeMbvJQ6b55U9MZQ1H/exec'
-      const form = document.forms['submit-to-google-sheet']
-      const msg = document.getElementById('msg')
-
-      form.addEventListener('submit', e => {
-        e.preventDefault()
-        fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-          .then(response =>{
-            msg.innerHTML = "Imformation sent to Siddharth Tiwari"
-            setTimeout(function(){
-              msg.innerHTML = " "
-            },1000)
-            form.reset()
-          })
-          .catch(error => console.error('Error!', error.message))
-      })
-
-      
-    var typed = new Typed('#element', {
-      strings: ['Web Designer !!' , 'UI/UX Designer !!' , ' Full Stack Developer !!' , 'Graphic Designer !!'],
-      typeSpeed: 50,
-    });
+var typed = new Typed("#element", {
+  strings: [
+    "Web Designer !!",
+    "UI/UX Designer !!",
+    " Full Stack Developer !!",
+    "Graphic Designer !!",
+  ],
+  typeSpeed: 50,
+});
